7245502c199614ff5628320b5236c434ca1980f2
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-03-08 18:59:03 +0300

    get All task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('tasks')
 export class TaskController {
   constructor(private taskService: TaskService) {}
 
+  @Get()
+  async getAll() {
+    const tasks = await this.taskService.getAllTasks();
+    return tasks;
+  }
   @Post()
   async create(
     @Body() data: { title: string; description: string; projectId: number },
